d2ee9ed03f2df2316b64bf71d7a8ce166bba92ac
Rocky Shao <122405175+Rocky0Shao@users.noreply.github.com>
2025-11-18 22:07:27 -0500

    1014/1157  cases passed
    Changed looping index errors.

--- 6. Zigzag Conversion.py
+++ 6. Zigzag Conversion.py
@@ -1,15 +1,18 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+          return s
         '''
         paypalishiring, num = 3
-        out_arr = 
+        out_arr =
            [[pay] -> zigzag = False
             [0p0] -> zigzag = True
             [ali]
             [0s0]
             [hir]
             [0i0]
-            [ng0]]
+            [ng0]
+            [000]]
         out = ""
         for col in out_arr:
             for char in col:
@@ -17,6 +20,7 @@ class Solution:
                     out += char
         return out
         '''
+
 
         #this below initlize out_arr to all zeros
         out = ""
@@ -27,37 +31,62 @@ class Solution:
             for j in range(numRows):
                 temp_row.append(0)
             out_arr.append(temp_row)
-        
-        row_index = 0
-        col_index = 0
-        s_index = 0
-        zigzag = False
-        col_max = numRows
+        # for row in out_arr:
+        #     print(row)
+
+        #change s from input string to list containing each char of input string
         temp = s
         s= []
         for i in temp:
             s.append(i)
+           
+        row_index = 0
+        col_index = 0
+       
+        zigzag = False
+        col_max = numRows
+
+
         while s:
-            print(f"row: {row_index}, col: {col_index}")
-            curr_char = s.pop(s_index) #return and rmove first char from input string
-            if not zigzag:
-                out_arr[row_index][col_index] = curr_char
-                col_index +=1
-                if col_index == col_max: #col_max == 3, lead to out bound
+            curr_char = s.pop(0)
+            try:
+                print(f"itr:  row: {row_index}  col: {col_index} zig:{zigzag} s:{s}\n")
+
+                
+                 #return and rmove first char from input string
+                if not zigzag:
+                    out_arr[row_index][col_index] = curr_char
+                    col_index +=1
+                    if col_index == col_max: #col_max == len(row), lead to out bound
+                        col_index -=1 #-=2 ensures from len(rows) to last_index -1
+                        # row_index +=1 #move down a row
+                        zigzag = True
+                if zigzag:                                       
+                    out_arr[row_index][col_index] = curr_char
                     col_index -=1
-                    zigzag = True
-            if zigzag:
-                col_index -=1
-                row_index +=1
-                out_arr[row_index][col_index] = curr_char
-                if col_index == 0:
-                    zigzag = False
+                    row_index +=1
+                    if col_index <= 0 or row_index > len(out_arr) -1:
+                        zigzag = False
+            except:
+                print("failed below")
+                print(f"itr:  row: {row_index}  col: {col_index} zig:{zigzag} \n")
         for row in out_arr:
             print(row)
-                
+        for i in range(col_max):
+            for row in out_arr:
+                if row[i] != 0:
+                    out += row[i]
+        return out
+               
 
 
 
 
 
-        
+
+
+
+
+
+       
+
